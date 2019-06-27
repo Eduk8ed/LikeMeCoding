814e7aa4746efba4d872c3b12ceeff4024182aa6
Shantell Barge <Shantellbarge@gmail.com>
2019-06-27 10:14:13 -0700

    add coders pic

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
+window.onload = () => {
+  document.querySelectorAll('.item').forEach(
+      item => item.addEventListener("click", handleClick)
+  )
 
+  document.querySelector('.close').addEventListener("click", () => {
+      document.querySelector(".big-panel").classList.remove("show")
+  })
+  
+}
+
+const handleClick = (event) => {
+  if (event.target.src) {
+      document.querySelector(".big-panel img").src = event.target.src
+      document.querySelector(".big-panel").classList.add("show")        
+  }
+
+}
   
